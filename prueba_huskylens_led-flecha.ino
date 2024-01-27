dca6999b201b64e5f46df780e7f1b7d7d952377a
Manuel Valencia <manuelvalencia2009@gmail.com>
2024-01-27 15:58:51 +0100

    Add files via upload

---
+++ prueba_huskylens_led-flecha.ino
@@ -0,0 +1,96 @@
+//
+// Información sobre la placa HuskyLens y el código en:
+// https://wiki.dfrobot.com/HUSKYLENS_V1.0_SKU_SEN0305_SEN0336
+//
+
+#include "HUSKYLENS.h"
+#include "SoftwareSerial.h"
+
+// Define huskylens como una instancia de la clase HUSKYLENS para interactuar con la placa.
+HUSKYLENS huskylens;
+
+// Configura una conexión serial de software en los pines 10 y 11.
+SoftwareSerial mySerial(10, 11); // Pin 10 conectado a RX de HUSKYLENS, Pin 11 a TX.
+
+// Prototipo de la función que imprime los resultados de HuskyLens.
+void printResult(HUSKYLENSResult result);
+
+const int ledPin1 = 3; // LED conectado al pin 3
+const int ledPin2 = 4; // LED conectado al pin 4
+
+void setup() {
+    // Inicia la comunicación serial con la computadora.
+    Serial.begin(9600);
+    // Inicia la comunicación serial con HuskyLens.
+    mySerial.begin(9600);
+
+    // Intenta iniciar la comunicación con HuskyLens hasta que tenga éxito.
+    while (!huskylens.begin(mySerial)) {
+        Serial.println(F("¡Ha fallado!"));
+        Serial.println(F("1. Asegúrate de que la HUSKYLENS esté configurada en: (General Settings>>Protocol Type>>Serial 9600)"));
+        Serial.println(F("2. Comprueba las conexiones de nuevo."));
+        delay(100);
+    }
+    
+    // Configura los pines de los LED como salidas
+    pinMode(ledPin1, OUTPUT);
+    pinMode(ledPin2, OUTPUT);
+}
+
+void loop() {
+    // Solicita datos a HuskyLens.
+    if (!huskylens.request()) {
+        Serial.println(F("Fallo al pedir datos a la HUSKYLENS, comprueba las conexiones."));
+    } else {
+        // Procesa los datos recibidos si hay algo para aprender o reconocer.
+        if (!huskylens.isLearned()) {
+            Serial.println(F("Nada que aprender, pulsa el botón de aprendizaje en la HUSKYLENS."));
+        } else if (!huskylens.available()) {
+            Serial.println(F("No hay bloques ni flechas detectados."));
+        } else {
+            // Imprime los resultados mientras haya datos disponibles.
+            Serial.println(F("###########"));
+            while (huskylens.available()) {
+                HUSKYLENSResult result = huskylens.read();
+                printResult(result);
+            }
+        }
+    }
+}
+
+// Imprime los detalles del objeto o flecha detectada por HuskyLens.
+void printResult(HUSKYLENSResult result) {
+    if (result.command == COMMAND_RETURN_BLOCK) {
+        Serial.print(F("Bloque: xCentro="));
+        Serial.print(result.xCenter);
+        Serial.print(F(", yCentro="));
+        Serial.print(result.yCenter);
+        Serial.print(F(", Ancho="));
+        Serial.print(result.width);
+        Serial.print(F(", Alto="));
+        Serial.print(result.height);
+        Serial.print(F(", ID="));
+        Serial.println(result.ID);
+    } else if (result.command == COMMAND_RETURN_ARROW) {
+        Serial.print(F("Flecha: xOrigen="));
+        Serial.print(result.xOrigin);
+        Serial.print(F(", yOrigen="));
+        Serial.print(result.yOrigin);
+        Serial.print(F(", xDestino="));
+        Serial.print(result.xTarget);
+        Serial.print(F(", yDestino="));
+        Serial.print(result.yTarget);
+        Serial.print(F(", ID="));
+        Serial.println(result.ID);
+        //Si la dirección de la fecha indica a la mitad izquierda de la pantalla encendemos el LED1, en caso contrario el LED2
+        if (result.xTarget < 160) {
+          digitalWrite(ledPin1, HIGH); // Enciende el LED en el pin 3
+          digitalWrite(ledPin2, LOW);  // Apaga el LED en el pin 4
+        } else {
+          digitalWrite(ledPin1, LOW);  // Apaga el LED en el pin 3
+          digitalWrite(ledPin2, HIGH); // Enciende el LED en el pin 4
+        }
+    } else {
+        Serial.println(F("Objeto desconocido."));
+    }
+}
